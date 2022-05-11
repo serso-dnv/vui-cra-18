c9d6df0e56c86d3e9e009a5ed56028f669da2784
Sokurenko <Sergii.Sokurenko@dnv.com>
2022-05-11 17:49:16 +0200

    Added @veracity/vui and guid generator demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
-import "./App.css";
-import { guid } from "@veracity/ui-utils";
+import { Box, Button, Modal, List, ListItem, showSuccess } from '@veracity/vui'
+import { guid, useToggle, copyToClipboard } from '@veracity/ui-utils'
 
-function App() {
+const App = () => {
+  const [showModal, toggleModal] = useToggle(false)
+
+  const copy = (text: string) =>
+    copyToClipboard(text).then(() =>
+      showSuccess(`Copied to the clipboard: ${text}`)
+    )
+
   return (
-    <div>
-      <ul>
-        {Array(100)
-          .fill("")
-          .map((g) => guid())
-          .map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-      </ul>
-    </div>
-  );
+    <Box p={4}>
+      <Button onClick={toggleModal}>Generate GUIDs</Button>
+      <Modal disableBackdropClose isOpen={showModal} onClose={toggleModal}>
+        <Box
+          bg="white"
+          borderRadius="md"
+          boxShadow="2"
+          centerH
+          column
+          left="50%"
+          outline="none"
+          p={2}
+          position="absolute"
+          top="50%"
+          transform="translate(-50%, -50%)">
+          <Box p={2}>
+            <List>
+              {Array(10)
+                .fill('')
+                .map(() => guid())
+                .map(g => (
+                  <ListItem key={g} justifyContent="space-between">
+                    {g}
+                    <Button
+                      onClick={() => copy(g)}
+                      icon="falCopy"
+                      size="sm"
+                      variant="text"
+                    />
+                  </ListItem>
+                ))}
+            </List>
+          </Box>
+          <Button onClick={toggleModal}>OK</Button>
+        </Box>
+      </Modal>
+    </Box>
+  )
 }
 
-export default App;
+export default App
+
